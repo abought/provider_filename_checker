f82bc95363358efc0d2dcb5a5b74ee7f039f4bec
Andy Boughton <abought@gmail.com>
2017-04-05 16:08:24 -0400

    Remove internal-only provider names

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,14 +4,16 @@ except ImportError:
     print('Must define local settings. Did you remember to copy settings/local-dist.py and update values?')
     raise
 
-# Things we should know about waterbutler
+#### Things we should know about waterbutler
+# Intentionally exclude: Rackspace cloudfiles, filesystem (used internally only),
+
+# MattF can provide owncloud credentials. For s3 testing, use your own amazon account. For FigShare, use https or
+#   personal token for oauth.
 KNOWN_PROVIDERS = [
     'box',
-    'cloudfiles',
     'dataverse',
     'dropbox',
     'figshare',
-    'filesystem',
     'github',
     'googledrive',
     'osfstorage',
