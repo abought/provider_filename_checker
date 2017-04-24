1b5af23de82230d57a108d963ea245a8af7d9228
Andy Boughton <abought@gmail.com>
2017-04-24 16:04:34 -0400

    GDrive with multiparts for metadata / parent folders

--- providers/googledrive/__init__.py
+++ providers/googledrive/__init__.py
@@ -3,15 +3,19 @@ import json
 import typing
 import urllib.parse
 
+import aiohttp
+
 from ..base import OauthBaseProvider
 import settings
+
 
 class GoogleDriveProvider(OauthBaseProvider):
     NAME = 'googledrive'
 
     DEFAULT_CREDENTIAL = settings.GOOGLEDRIVE_OAUTH_TOKEN
     BASE_URL = 'https://www.googleapis.com/drive/v2/'  # TODO: v3 exists but wb uses v2
-    BASE_CONTENT_URL = 'https://www.googleapis.com/upload/drive/v2'
+    BASE_CONTENT_URL = 'https://www.googleapis.com/upload/drive/v2/files'
+
     async def create_folder(self, foldername: str):
         """See https://developers.google.com/drive/v3/web/folder"""
         parent_folder = self.parent_folder or 'root'
@@ -33,27 +37,38 @@ class GoogleDriveProvider(OauthBaseProvider):
                           filename: str,
                           content) -> typing.Tuple[dict, int]:
         """
-        See https://developers.google.com/drive/v2/web/simple-upload
-        Unlike WB, we will use simple upload method
+        See https://developers.google.com/drive/v2/web/multipart-upload
+        (use multipart for simple, non-resumable uploads with metadata)
         """
-
-        #
         parent_folder = self.parent_folder or 'root'
         # Upload files to a different host than the api base url
         url = self.BASE_CONTENT_URL
 
         size = len(content.encode('utf-8'))
         params = {
-            'uploadType': 'media'
+            'uploadType': 'multipart'
         }
         headers = {
             'Content-Type': 'text/plain',
             'Content-Length': str(size)
         }
 
-        return await self._make_request('POST', url, data=content, params=params, headers=headers)
+        # Construct multipart payload
+        with aiohttp.MultipartWriter('related') as mpwriter:
+            mpwriter.append_json(
+                {
+                    'title': filename,
+                    'parents': [{
+                        'kind': 'drive#parentReference',
+                        'id': parent_folder
+                    }]
+                },
+                headers={'charset': 'UTF-8'}
+            )
+            mpwriter.append(content, headers={'Content-Type': 'text/plain'})
+            return await self._make_request('POST', url, data=mpwriter, params=params, headers=headers)
 
     @staticmethod
-    def extract_uploaded_filename(payload: dict=None):
-        #TODO: Implement
-        return payload['name']
+    def extract_uploaded_filename(payload: dict = None):
+        # TODO: Implement
+        return payload['title']
